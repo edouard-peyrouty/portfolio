974c43814efbb18f897dd95a336f10b5c8047d4f
edouard.peyrouty <edouard.peyrouty@gmail.com>
2026-01-22 10:56:38 +0100

    version 1.0.0.0 : première publication

---
+++ assets/js/layout.js
@@ -0,0 +1,38 @@
+fetch("/partials/header.html")
+  .then(res => res.text())
+  .then(data => {
+    document.getElementById("header").innerHTML = data;
+    setActiveNav();
+  })
+  .catch(console.error);
+
+fetch("/partials/footer.html")
+  .then(res => res.text())
+  .then(data => {
+    const footer = document.getElementById("footer");
+    if (footer) footer.innerHTML = data;
+  })
+  .catch(() => {})
+  .finally(() => {
+    document.querySelector("main").style.opacity = "1";
+  });
+
+
+// Fonction pour définir le bouton de navigation actif
+function setActiveNav() {
+    console.log(window.location.pathname.split("/")[1]);
+    const currentPage = window.location.pathname.split("/")[1] || 'index.html';
+    const navButtons = document.querySelectorAll(".nav-btn");    
+    navButtons.forEach(button => {
+        const buttonPage = button.getAttribute("href").split("/")[1];
+        if (buttonPage === currentPage) {
+            button.classList.add("active");
+        } else {
+            button.classList.remove("active");
+        }   
+        // Ajoute le onclick à chaque bouton qui réduit l'opacité du main
+        button.addEventListener("click", function() {            
+            document.getElementsByClassName("main")[0].style.opacity = "0";
+        });    
+    });
+}
